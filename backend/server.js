97038cf7f0f5e7bd461f4d7d689f51df5b26748a
jahnavi02 <58691610+jahnavi02@users.noreply.github.com>
2024-02-17 21:15:00 +0530

    Update server.js email

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.post('/send-email', async (req, res) => {
 
         // send mail with defined transport object
         let info = await transporter.sendMail({
-            from: 'testsellerkin@gmail.com',
+            from: 'jahnavi2023.freelance@gmail.com',
             to: to,
             subject: 'Subscription confirmation',
             text: `Hello ${firstName}, Thanks for Subscribing ` // plain text body
